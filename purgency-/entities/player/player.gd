8d945ff03b74d6d6c6586ced0d72cd33d7497fe6
Dakari Sow <dddddakari@gmail.com>
2025-05-14 23:34:20 -0600

    Idle + walking  + animated sprites when walking
    I FIGURED IT OUT!! Sprite will now walk in different directions freely without falling off map and also animated sprite of each direction functions.

--- purgency-/entities/player/player.gd
+++ purgency-/entities/player/player.gd
@@ -1,25 +1,67 @@
 extends CharacterBody2D
 
+const SPEED = 300
+var current_dir = "none"
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(delta):
+	player_movement(delta)
+	
+func player_movement(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = -SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
+		velocity.y = SPEED
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
+		velocity.y = -SPEED
+		velocity.x = 0
+	else: 
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+		
 	move_and_slide()
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $Sprite2D
+	
+	if dir == "right":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("walking_left")
+		elif movement == 0:
+			anim.play("to_the_side")
+	elif dir == "left":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("walking_left")
+		elif movement == 0:
+			anim.play("to_the_side")
+	elif dir == "up":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("walking_back")
+		elif movement == 0:
+			anim.play("back_idle")
+	elif dir == "down":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("walking")
+		elif movement == 0:
+			anim.play("default")
+	
+	
