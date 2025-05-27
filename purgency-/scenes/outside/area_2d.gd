612cead5e103548a5d98f624179d2ff9aa4c9cb5
maximL5 <maxim.lopez@edu.sait.ca>
2025-05-27 10:04:19 -0600

    new game repathed

--- purgency-/scenes/outside/area_2d.gd
+++ purgency-/scenes/outside/area_2d.gd
@@ -10,5 +10,4 @@ func _on_body_exited(_body):
 
 func _process(_delta):
 	if entered == true: 
-		if Input.is_action_just_pressed("interact"):
-			get_tree().change_scene_to_file("res://background/kitchen/kitchen01.tscn")
+		get_tree().change_scene_to_file("res://scenes/outside/black.jpg")
