be30ee7c2da9e140d1f80753acd27fca270d32e2
Dakari Sow <dddddakari@gmail.com>
2025-06-03 08:45:39 -0600

    Inventory and Sprite

--- purgency-/inventorysystem/inventoryslot.gd
+++ purgency-/inventorysystem/inventoryslot.gd
@@ -1,20 +1,18 @@
 extends Control
 
-@onready var icon: TextureButton = $TextureButton
+@onready var icon: TextureRect = $TextureRect
 @onready var quantity_label: Label = $Label
 
 var item: Dictionary = {}
 
-func set_item(new_item: Dictionary) -> void:
+func set_item(new_item: Dictionary):
 	item = new_item
-	icon.texture_normal = item.get("icon", null) 
+	icon.texture = item.get("icon", null)
 	quantity_label.text = str(item.get("quantity", 1))
 	show()
 
-
-
-func clear_slot() -> void:
+func clear_slot():
 	item = {}
-	icon.texture_normal = null
+	icon.texture = null
 	quantity_label.text = ""
 	hide()
