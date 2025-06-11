2d9ffaee886fbe07de541d04ce1d3cdb7b3c4b94
Dakari Sow <dddddakari@gmail.com>
2025-06-10 18:37:05 -0600

    Squashed commit of the following:
    commit 4a57120093762e2dd7cf3fd288eb586e82ec9a96
    Author: maximL5 <maxim.lopez@edu.sait.ca>
    Date:   Tue Jun 10 18:26:19 2025 -0600
    
        ver1 of maze minigame done

---
+++ purgency-/minigames/maze/maze_ui.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var entered = false
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	if _body.name == "Player": 
+		get_tree().change_scene_to_file.call_deferred("res://minigames/maze/Jumpscare.tscn")
+
+	
+
+func _on_exit_body_entered(_body: Node2D) -> void:
+	if _body.name == "Player":
+		get_tree().change_scene_to_file.call_deferred("res://scenes/hospital/f1_rooms_area/RoomsArea.tscn")
