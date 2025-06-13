95253134c427201743e0c18594cc93bc05af5b13
maximL5 <maxim.lopez@edu.sait.ca>
2025-06-13 14:15:59 -0600

    8-bit finn added to maze

--- purgency-/minigames/maze/maze_ui.gd
+++ purgency-/minigames/maze/maze_ui.gd
@@ -3,11 +3,11 @@ extends Node2D
 var entered = false
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
-	if _body.name == "Player": 
+	if _body.name == "CharacterBody2D": 
 		get_tree().change_scene_to_file.call_deferred("res://minigames/maze/Jumpscare.tscn")
 
 	
 
 func _on_exit_body_entered(_body: Node2D) -> void:
-	if _body.name == "Player":
+	if _body.name == "CharacterBody2D":
 		get_tree().change_scene_to_file.call_deferred("res://scenes/hospital/f1_rooms_area/RoomsArea.tscn")
